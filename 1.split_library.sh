f6a8c1d26286a0bed1361829e265413a14540809
Erika <ekeim@uw.edu>
2016-05-03 11:54:02 -0700

    commit for splitting libraries

---
+++ 1.split_library.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+split_libraries.py -m 
+040816JP27F-mapping.txt -f 
+data_raw/fasta-qual-mapping-files/040816JP27F-full.fasta -q 
+data_raw/fasta-qual-mapping-files/040816JP27F-full.qual -o split_library_output -b 8
