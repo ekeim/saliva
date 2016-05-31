15e11b1ec8d4f2fded8235685d7f9b3e9335618e
Erika <ekeim@uw.edu>
2016-05-31 08:59:43 -0700

    alpha diversity- not quite working need to breakdown into discrete commands heatmap- heatmaps at higher taxonomic levels

--- 6.Make_otu_heatmap.sh
+++ 6.Make_otu_heatmap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
+make_otu_heatmap.py -i taxa_summary/otu_table_L5.biom -o taxa_summary/otu_table_ek5_heatmap.pdf -c source -m 040816JP27F-mapping.txt \
 
-make_otu_heatmap.py -i otus/otu_table.biom -o otu_table_ek_heatmap.pdf -c source -m 040816JP27F-mapping.txt
 
